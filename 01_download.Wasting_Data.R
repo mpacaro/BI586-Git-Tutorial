08332806943b094cba74b74ef281ac29741be7b7
Maddy <mpacaro@bu.edu>
2021-02-15 13:45:05 -0500

    Added title to boxplot

--- 01_download.Wasting_Data.R
+++ 01_download.Wasting_Data.R
@@ -2,5 +2,5 @@ install.packages("readr")
 library(readr)
 data <- read_csv("https://raw.githubusercontent.com/daviessw/BI586/master/R_Worksheet/Wasting_Data.csv", skip=1)
 library(ggplot2)
-boxplot(data$`Average WD (%)`~Transect,data=data, main="not sure what to put here",
+boxplot(data$`Average WD (%)`~Transect,data=data, main="Average Wasting Disease Percentage per Transect",
         xlab="Transect", ylab="Average wasting disease %")
